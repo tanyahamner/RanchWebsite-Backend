1f3c9526e971ccad959431237cd247859a8c800b
ryanlowee <ryan@devpipeline.com>
2021-08-10 14:04:13 -0600

    Adds way to upload images

--- endpoints/pic_add.py
+++ endpoints/pic_add.py
@@ -1,20 +1,44 @@
 from flask import jsonify
 import flask
-import os, sys
+import glob, os
+import uuid
 from db import db
 from models.app_users import AppUser, user_schema
+from models.pic_reference import PicReference, pic_schema
 from PIL import Image
 import io
 from lib.authenticate import authenticate_return_auth
+import base64
 
 @authenticate_return_auth
 def pic_add(req:flask.Request, auth_info) -> flask.Response:
     post_data = req.get_json()
     file_bytes = post_data.get('pic_bytes')
+    original_name = post_data.get('file_name')
+    ext = post_data.get('ext')
+    base_64_file_bytes = file_bytes.encode('utf-8')
+    file_id = uuid.uuid4()
+    user_id = auth_info.user.user_id
 
-    path = os.getcwd() + "/endpoints/uploads/images"
+    path = os.getcwd() + "/endpoints/uploads/images/" + f'{file_id}.{ext}'
+    thumb_path = os.getcwd() + "/endpoints/uploads/images/thumbnails/" + f'{file_id}'
     
-    with open(path, "ab") as output:
-        output.write(file_bytes)
+    with open(path, 'wb') as output:
+        decoded_image = base64.decodebytes(base_64_file_bytes)
+        output.write(decoded_image)
 
-    return
+    size = 90, 90
+
+    for infile in glob.glob(path):
+        file, ext = os.path.splitext(thumb_path)
+        with Image.open(infile) as im:
+            im.thumbnail(size)
+            im.save(file + ".JPEG")
+
+
+        record = PicReference(file_id, user_id, original_name, ext)
+
+        db.session.add(record)
+        db.session.commit()
+ 
+    return jsonify('Uploaded image.'), 200
--- models/pic_reference.py
+++ models/pic_reference.py
@@ -5,21 +5,20 @@ from db import db
 import marshmallow as ma
 
 class PicReference(db.Model):
-    pic_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    pic_id = db.Column(UUID(as_uuid=True), primary_key=True, unique=True, nullable=False)
     user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('app_user.user_id'), nullable=False)
     original_name = db.Column(db.String(), nullable = False)
-    temp_name = db.Column(db.String(), nullable = False)
-    file_size = db.Column(db.String(), nullable = False, unique = True)
+    ext = db.Column(db.String(), nullable = False)
 
-    def __init__(self, user_id, original_name, temp_name, file_size):
+    def __init__(self, pic_id, user_id, original_name, ext):
+        self.pic_id = pic_id
         self.user_id = user_id
         self.original_name = original_name
-        self.temp_name = temp_name
-        self.file_size = file_size
+        self.ext = ext
    
 class PicReferenceSchema(ma.Schema):
     class Meta:
-        fields = ['pic_id', 'user_id', 'original_name', 'temp_name', 'file_size']
+        fields = ['pic_id', 'user_id', 'original_name', 'ext']
     
 pic_schema = PicReferenceSchema()
-pic_schema = PicReferenceSchema(many=True)
+pics_schema = PicReferenceSchema(many=True)
