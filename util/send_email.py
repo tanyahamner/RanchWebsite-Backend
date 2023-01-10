174cce751a3c8e06e96fccfa7677986f28e95e84
Ammon Roy <ammonroy@dp-c02yg39bjg5h.localdomain>
2023-01-10 09:18:46 -0700

    created a .env file and utalized dotenv to remove hardcoded values

--- util/send_email.py
+++ util/send_email.py
@@ -2,19 +2,19 @@ from sendgrid import SendGridAPIClient
 import os
 from sendgrid.helpers.mail import Mail, Email, To, Content
 
-API_KEY = os.environ.get('SENDGRID_API_KEY', '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')
-FROM_EMAIL = os.environ.get('SENDGRID_FROM_EMAIL', 'foundation@devpipeline.com')
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+SENDGRID_FROM_EMAIL = os.getenv('SENDGRID_FROM_EMAIL')
 
 def send_email(to_email, subject, content):
-
-    sg = SendGridAPIClient(api_key=API_KEY)
+    if not SENDGRID_API_KEY and not SENDGRID_FROM_EMAIL:
+        return 'Exception Occured: Cannot send email without .env varibles'
+    sg = SendGridAPIClient(api_key=SENDGRID_API_KEY)
     message = Mail(
-        from_email=FROM_EMAIL,
+        from_email=SENDGRID_FROM_EMAIL,
         to_emails=to_email,
         subject=subject,
         html_content=content
     )
 
     response = sg.send(message)
-    print(response.status_code)
-    print(response.headers)
+
